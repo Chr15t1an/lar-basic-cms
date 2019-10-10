66490c971a69ef21d58dfb0174fad1e8f550bc6b
Chr15t1an <ccampbell@sapphirebd.com>
2019-10-10 10:41:17 -0500

    Create MetaDeleteTest.php
    Still need to Add delete :+1:

---
+++ tests/Feature/MetaDeleteTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class MetaDeleteTest extends TestCase
+{
+  use WithFaker, DatabaseTransactions, WithoutMiddleware;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function test_read_meta()
+    {
+
+      // Create key & Values
+      $attributes = [ // sentence
+        'meta_key' => $this->faker->word,
+        'meta_value' => $this->faker->word,
+      ];
+
+
+      //Add to database
+        $this->json('POST','api/meta/add',$attributes);
+
+      // Check that it exists
+        $this->assertDatabaseHas('metavalues',$attributes);
+
+
+
+        //Delete it
+
+        $this->json('POST','api/meta/delete',[
+                    'meta_key' => $attributes['meta_key'],
+                ]);
+
+        $this->assertDatabaseMissing('metavalues', $attributes);
+
+    }
+}
+
+
+// Route::post('/meta/add','MetavalueController@add_metadata')->middleware('auth:api');
+// Route::post('/meta/get','MetavalueController@api_get_metadata');
+// Route::post('/meta/update','MetavalueController@api_update')->middleware('auth:api');
+// Route::post('/meta/delete','MetavalueController@destroy')->middleware('auth:api');
