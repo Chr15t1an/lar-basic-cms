1fce752d3d919bd4e693b87500d335b7ab237341
Chr15t1an <ccampbell@sapphirebd.com>
2019-11-06 08:12:04 -0600

    Create Files CRUD Paths
    & MVC=s

--- routes/api.php
+++ routes/api.php
@@ -45,10 +45,17 @@ Route::post('/meta/delete','MetavalueController@destroy')->middleware('auth:api'
 //Generate a sitemap
 Route::get('/admin/settings/seo/gen-sitemap', 'AdminController@makeSitemap')->middleware('auth:api');
 
-//Create A Post
-Route::get('/admin/posts', 'PostController@api_index');//->middleware('auth:api');
-// Route::post('/admin/posts/{slug}', 'PostController@index')->middleware('auth:api');
-Route::post('/admin/posts/store', 'PostController@store');//->middleware('auth:api');
+//Posts
+Route::get('/admin/posts', 'PostController@api_index')->middleware('auth:api');
+Route::post('/admin/posts/store', 'PostController@store')->middleware('auth:api');
 Route::get('/admin/posts/edit/{id}', 'PostController@edit')->middleware('auth:api');
 Route::post('/admin/posts/edit/{id}', 'PostController@update')->middleware('auth:api');
 Route::post('/admin/posts/delete/{id}', 'PostController@destroy')->middleware('auth:api');
+
+
+//Files
+// Route::get('/files', 'FileController@api_index')->middleware('auth:api');
+Route::post('/file/store', 'FileController@store');//->middleware('auth:api');
+Route::get('/file/edit/{id}', 'FileController@edit')->middleware('auth:api');
+Route::post('/file/edit/{id}', 'FileController@update')->middleware('auth:api');
+Route::post('/file/delete/{id}', 'FileController@destroy')->middleware('auth:api');
