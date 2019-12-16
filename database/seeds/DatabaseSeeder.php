daa253afab9461376926014ada66d5e7fcf2d2e5
Chr15t1an <ccampbell@sapphirebd.com>
2019-12-16 04:41:48 -0600

    Update DatabaseSeeder.php
    Update database q's

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         //Todo Checklist
         $newMeta = new Metavalue;
         $newMeta->meta_key = 'checklist';
-        $newMeta->meta_value = '{"CHECKLIST":{"Items":[{"NAME":"Uptime Robot","STATE":true},{"NAME":"Cloudflare","STATE":false},{"NAME":"Google Tag Manager","STATE":false},{"NAME":"Google Analitics","STATE":false},{"NAME":"Turn on ecommerce tracking | Google Analitics","STATE":true},{"NAME":"Bugsnag","STATE":false},{"NAME":"Generate Sitemap","STATE":false},{"NAME":"Robot.txt","STATE":false},{"NAME":"Check H1s and Metas","STATE":false},{"NAME":"Set up Conversions Events","STATE":false},{"NAME":"FavIcon - https://favicon.io/favicon-converter/","STATE":false},{"NAME":"Mixpanel.com","STATE":false},{"NAME":"Disable Public Registration","STATE":false}]}}';
+        $newMeta->meta_value = '{"CHECKLIST":{"Items":[{"NAME":"Uptime Robot","STATE":true},{"NAME":"Cloudflare","STATE":false},{"NAME":"Google Tag Manager","STATE":false},{"NAME":"Google Analitics","STATE":false},{"NAME":"Turn on ecommerce tracking | Google Analitics","STATE":true},{"NAME":"Bugsnag","STATE":false},{"NAME":"Generate Sitemap","STATE":false},{"NAME":"Robot.txt","STATE":false},{"NAME":"Check H1s and Metas","STATE":false},{"NAME":"Set up Conversions Events","STATE":false},{"NAME":"FavIcon - https://favicon.io/favicon-converter/","STATE":false},{"NAME":"Mixpanel.com","STATE":false},{"NAME":"Disable Public Registration","STATE":false},{"NAME":"Add to Search Console","STATE":false}]}}';
         $newMeta->save();
 
 
