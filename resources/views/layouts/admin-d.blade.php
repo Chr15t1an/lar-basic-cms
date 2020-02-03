7a6020957c785bd4ca545e848ce6dd36cd0fee16
Chr15t1an <ccampbell@sapphirebd.com>
2020-02-03 10:43:03 -0600

    Update admin-d.blade.php
    Remove old & Excess HTML from Template.

--- resources/views/layouts/admin-d.blade.php
+++ resources/views/layouts/admin-d.blade.php
@@ -12,12 +12,6 @@
 <!-- Fonts -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet">
-
-<!-- https://material.io/resources/icons/?search=delete&icon=commute&style=baseline
-
-           -->
-    <!-- include summernote css/js -->
-
     <title>@yield('title')</title>
   </head>
 <body>
@@ -100,17 +94,6 @@
               </div>
             </div>
           </div>
-          <!-- Form -->
-          <form class="mt-4 mb-3 d-md-none">
-            <div class="input-group input-group-rounded input-group-merge">
-              <input type="search" class="form-control form-control-rounded form-control-prepended" placeholder="Search" aria-label="Search">
-              <div class="input-group-prepend">
-                <div class="input-group-text">
-                  <span class="fa fa-search"></span>
-                </div>
-              </div>
-            </div>
-          </form>
           <!-- Navigation -->
           <ul class="navbar-nav">
             <li class="nav-item  active ">
@@ -165,45 +148,8 @@
                 <i class="ni ni-single-02 text-yellow"></i>Settings
               </a>
             </li>
-
-            <!-- <li class="nav-item">
-              <a class="nav-link " href="./examples/tables.html">
-                <i class="ni ni-bullet-list-67 text-red"></i> Tables
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./examples/login.html">
-                <i class="ni ni-key-25 text-info"></i> Login
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./examples/register.html">
-                <i class="ni ni-circle-08 text-pink"></i> Register
-              </a>
-            </li> -->
           </ul>
-          <!-- Divider -->
-          <!-- <hr class="my-3"> -->
-          <!-- Heading -->
-          <!-- <h6 class="navbar-heading text-muted">Documentation</h6> -->
-          <!-- Navigation -->
-          <!-- <ul class="navbar-nav mb-md-3">
-            <li class="nav-item">
-              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html">
-                <i class="ni ni-spaceship"></i> Getting started
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html">
-                <i class="ni ni-palette"></i> Foundation
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html">
-                <i class="ni ni-ui-04"></i> Components
-              </a>
-            </li>
-          </ul> -->
+
         </div>
       </div>
     </nav>
@@ -218,17 +164,7 @@
       <div class="container-fluid">
         <!-- Brand -->
         <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="./index.html">Dashboard</a>
-        <!-- Form -->
-        <form class="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
-          <div class="form-group mb-0">
-            <div class="input-group input-group-alternative">
-              <div class="input-group-prepend">
-                <span class="input-group-text"><i class="fas fa-search"></i></span>
-              </div>
-              <input class="form-control" placeholder="Search" type="text">
-            </div>
-          </div>
-        </form>
+      
         <!-- User -->
         <ul class="navbar-nav align-items-center d-none d-md-flex">
           <li class="nav-item dropdown">
