5ba4087ba0d1ea2172008422edc89409693a619c
Chr15t1an <ccampbell@sapphirebd.com>
2020-02-06 09:02:45 -0600

    Components HTML
    :+1:

--- resources/views/documentation/pubhome.blade.php
+++ resources/views/documentation/pubhome.blade.php
@@ -8,21 +8,46 @@
 <section class="row align-items-center">
 <div class="col-12">
 <h1>Market Test Template</h1>
-<p>This basic content management system serves as a starting point to test website ideas.
+<h2>What it is?</h2>
+<p>
+  This Laravel based template serves as a starting point to test website ideas. This was inspired by The Lean Startup by Eric Ries and is a part of a market test process I am developing here <a href="https://myofficeitperson.com/posts/test-a-business-idea-process">https://myofficeitperson.com/posts/test-a-business-idea-process</a>.
 </p>
-<p>This CMS has components in place to collect leads, calculate pricing, capture contact messages, and create blog posts.
+<h2>What you can do with it?
+</h2>
+<p>Install this repository and have all of the boilerplate lead acquisition stuff done. Add your analytics script, build the design and copy for a lead funnel, and start running traffic at it.
+If your goal is to test a value proposition, pricing and track everything using Google Tag Manager & Google Analytics. This install may serve you.
 </p>
-<p>If your goal is to test a value proposition, pricing and track everything using Google Tag Manager & Google Analytics. This install may serve you.
+<h2>Features
+</h2>
+<p>
+  This install has components in place to collect leads, calculate pricing, capture contact messages, and create blog posts.
 </p>
+<a href="/components"><button class="btn btn-lg btn-default">Components</button></a>
+<a href="/instalation"><button class="btn btn-lg btn-default"></button>Instalation</a>
+<a href="/demo"><button class="btn btn-lg btn-default"></button>Demo</a>
 
-<a class="btn btn-lg" href="/components">Components</a>
 
 
 <h3>Tech Stack</h3>
-<p>PHP, Javascript, MYSQL<br/>
-Vue JS & Laravel </p>
+<p>PHP, Javascript, MYSQL, HTML & CSS<br/>
+</p>
+<h3>Framework</h3>
+<p>
+Vue JS , Laravel, Bootstrap.</p>
+[icons]
+
 [Icon]
+<h3>Methodology</h3>
+
+<p>Test-Driven Development - PhpUnit</p>
+[Gif]
+
+
+
 </div>
+
+
+
 
 
 </section>
