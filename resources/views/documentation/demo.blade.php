bfe079858e8127faef48026d14c5f5d3b99d4ddc
Chr15t1an <ccampbell@sapphirebd.com>
2020-02-07 11:12:10 -0600

    Install Page
    :+1:

--- resources/views/documentation/demo.blade.php
+++ resources/views/documentation/demo.blade.php
@@ -12,7 +12,7 @@
 <h2>Video Demostration</h2>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/qStaxEpnj1M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-<p>You could always try <a href="/instillation">installing</a> it yourself</p>
+<p>You could always try <a href="/installation">installing</a> it yourself</p>
 
 </div>
 
--- resources/views/documentation/installation.blade.php
+++ resources/views/documentation/installation.blade.php
@@ -8,34 +8,25 @@
 
 <section class="row align-items-center">
 <div class="col-12">
-<h1>Installation</h1>
-<h2>Provision a server</h2>
-<p>
-Pull this repo
-https://github.com/Chr15t1an/provision-script-set/
-</p>
-<h2>Fill out info in this file and run ‘. init.sh’
-</h2>
-<p>https://github.com/Chr15t1an/provision-script-set/blob/master/init.sh
 
-</p>
-<h2>Run PHP Unit Tests to make sure everything is working
-</h2>
-<p>
-  ./vendor/bin/phpunit
-</p>
-
+  <h1 class="text-center">Installation</h1>
+  <div class="container">
+    <h2>1. Provision a server</h2>
+      <p class="lead">
+      Pull this repo
+      <code>https://github.com/Chr15t1an/provision-script-set/</code>
+      </p>
+    <h2>2. Fill out info in this file and run ‘. init.sh’</h2>
+      <p class="lead"><code>https://github.com/Chr15t1an/provision-script-set/blob/master/init.sh</code></p>
+      <p class="lead"> You will add the marketing template repository in the init.sh
+        In the provision script. Alterativly you could set the repo up like a normal Laravel install.
+        <code>https://github.com/Chr15t1an/lar-basic-cms.git</code></p>
+    <h2>3. Run PHP Unit Tests</h2>
+      <p class="lead">
+  <code>./vendor/bin/phpunit</code>
+      </p>
+    </div>
 </div>
-
-
-
-
-
-</section>
-
-
-
-
 
 </section>
 
